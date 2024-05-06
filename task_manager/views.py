b4420e7c4a186c41092ab73b831c31ab6f129430
Vadim Babii <judviiioff@gmail.com>
2024-05-06 15:28:09 +0300

    update settings, add index view and update urlpatterns

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Task, TaskType, Position
 
-# Create your views here.
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_tasks = Task.objects.count()
+    not_completed_tasks_count = Task.objects.filter(is_completed=False).count()
+
+    context = {
+        "num_tasks": num_tasks,
+        "not_completed_tasks_count": not_completed_tasks_count,
+    }
+
+    return render(
+        request, "task_manager/index.html",
+        context=context
+    )
