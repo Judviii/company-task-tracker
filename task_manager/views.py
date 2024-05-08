ac8b887c14feb1bea40b82e30eabc91bbadb8548
Vadim Babii <judviiioff@gmail.com>
2024-05-08 19:50:57 +0300

    add registration and authenticate, update settings✅

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Task, TaskType, Position
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from .models import Task, TaskType, Position, Worker
 
 
 def index(request):
@@ -17,3 +23,33 @@ def index(request):
         request, "task_manager/index.html",
         context=context
     )
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+
+    def get_queryset(self):
+        queryset = Worker.objects.order_by("username")
+        username = self.request.GET.get("username", "")
+        if username:
+            queryset = queryset.filter(username__icontains=username)
+        return queryset
+
+
+class WorkerDetailView(generic.DetailView):
+    model = Worker
+    queryset = Worker.objects.all()
+
+
+class WorkerCreateView(generic.CreateView):
+    model = Worker
+
+
+class WorkerUpdateView(generic.UpdateView):
+    model = Worker
+    success_url = reverse_lazy("")
+
+
+class WorkerDeleteView(generic.DeleteView):
+    model = Worker
+    success_url = reverse_lazy("task-manager:index")
