c9fad597252ddc174652e8cc79baf4157dc8835c
Vadim Babii <judviiioff@gmail.com>
2024-05-17 19:02:22 +0300

    update settings.py, add empty line in task_toggle_assign_form.html

--- company_task_tracker/settings.py
+++ company_task_tracker/settings.py
@@ -22,6 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
+# To run it locally, please use this variable:
+# SECRET_KEY = "django-insecure-0(njc*t7hc-ezawxtqanzfkykpo8&s#(@hs316%f8^ny0g3(jh"
+#
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
